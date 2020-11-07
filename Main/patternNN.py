5e96cd0ef6ac3cab2efe94e998a089b2c59538d2
SeungJin Lee <seungjin9777@gmail.com>
2020-11-08 02:16:43 +0900

    pattern모듈 수정

--- Main/patternNN.py
+++ Main/patternNN.py
@@ -6,32 +6,22 @@ from keras.models import load_model
 # pattern = pattern()
 # pattern.model_predict() --> ~무늬 혹은 알수없는 패턴 리턴.
 
-class pattern:
+class Pattern:
 
     def __init__(self):
-        self.img_dir = "'../data'"
+        #self.img_dir = "'../data'"
         self.model_dir = "../data/pattern_model.h5"
-        self.image_w = 64
-        self.image_h = 64
 
-    def image_processing(self):
-        img = self.img_dir + "/image.jpg"
-        img = Image.open(img)
-        img = img.convert("RGB")
-        img = img.resize((self.image_w, self.image_h))
-        img = np.asarray(img)
-        return img
-
-    def model_predict(self):
+    def image_predict(self):
         model = load_model(self.model_dir)
 
         prediction = model.predict(self.image_processing())
         np.set_printoptions(formatter={'float': lambda x: "{0:0.3f}".format(x)})
 
         for i, value in prediction:
-            classes_pattern = ["줄", '지그재그', '땡땡이', '꽃', '기본', '체크']
+            classes_pattern = ["줄무늬", '지그재그무늬', '땡땡이', '꽃무늬', '무지', '체크무늬']
             if value >= 0.8:
-                result = classes_pattern[i] + "무늬"
+                result = classes_pattern[i]
                 return result
 
         no_result = '알수없는 패턴'
