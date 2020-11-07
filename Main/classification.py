468347803a1929537d18930eb3d8b4cbe7dc4cbd
SeungJin Lee <seungjin9777@gmail.com>
2020-11-08 02:17:20 +0900

    3개모듈 병합완료

--- Main/classification.py
+++ Main/classification.py
@@ -1,20 +1,19 @@
 from multiprocessing import Process, Queue
 import color as color
 from camera import CameraModule
-
-# from patternNN import pattern
-# pattern = pattern()
-# pattern.model_predict() --> ~무늬 혹은 알수없는 패턴 리턴.
-
+from patternNN import Pattern
+from shapeNN import Shape
 
 class Classification:
     def __init__(self):
+        '''
+               shapeNN, patternNN = 인스턴스 생성
+               colorDetector = 클래스 인스턴스 생성
+         '''
         self.camera = CameraModule()
-        '''
-        shapeNN = 모델 불러오기
-        patternNN = 모델 불러오기
-        colorDetector = 클래스 인스턴스 생성
-        '''
+        self.pattern = Pattern()
+        self.shape = Shape()
+
 
     def execute(self):
         colorQ = Queue()
@@ -28,7 +27,15 @@ class Classification:
         color_process.join()
         color_string = colorQ.get()
 
-        return color_string
+        # 카메라모듈에서 전처리된 사진으로 각각 예측하여 스트링 반환.
+        pattern = self.pattern.image_predict(data_pattern)
+        shape = self.shape.image_predict(data_shape)
+
+        return color_string, pattern, shape
+
+
+
+
 
 
 
